142e925316b4efa285a4727f9d1de094b5c6ca98
CiksGg <kambarbekovamantur81@gmail.com>
2023-01-06 12:35:49 +0600

    test

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]
